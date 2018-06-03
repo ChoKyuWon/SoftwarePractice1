ef8602aa4e27fe23ab727b0546b3d7e427416824
ChoKyuWon <yyj10137@gmail.com>
2018-06-04 06:51:03 +0900

    make outline file

--- manager.java
+++ manager.java
@@ -7,16 +7,27 @@ import java.sql.SQLException;
 
 public class manager{
     public static void main(String argv[]){
-
+        int mode;
+        Scanner input=new Scanner(System.in);
         try{
             Connection con=null;
             con = DriverManager.getConnection("jdbc:mysql://localhost","root", "1234"); // 3rd arg=password
 
         }catch(SQLException sqex){
             System.out.println("SQLException: " + sqex.getMessage());
-			System.out.println("SQLState: " + sqex.getSQLState());
+            System.out.println("SQLState: " + sqex.getSQLState());
         }
-        
+
+        while(1){
+            mode=input.nextINT();
+            switch(mode){
+                case 1: insert(); break;
+                case 2: modify(); break;
+                case 3: delete(); break;
+                case 4: search(); break;
+                case 5: sort(); break;
+            }
+        }
     }
 
     public void insert(){
